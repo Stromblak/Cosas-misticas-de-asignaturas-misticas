56906d4e84ff0d57f7c0a0dd56bf40c229dd1f32
RyuLith <105754662+RyuLith@users.noreply.github.com>
2022-05-18 15:09:19 -0400

    Update t.cpp

--- t.cpp
+++ t.cpp
@@ -4,23 +4,7 @@ using namespace std;
 
 int main()  
 {
-  int t, s, a, f;
-  cin >> t;
-  while (t--)
-  {
-    cin >> s >> a >> f;
-    int street[f];
-    int avenue[f];
-    for (int i = 0; i < f; i++)
-      cin >> street[i] >> avenue[i];
-    sort(street, street + f);
-    sort(avenue, avenue + f);
-    int mid;
-    if (f % 2 == 0)
-     asd
-    else
-      mid = (f + 1) / 2 - 1;
-    cout << "(Street: " << street[mid] << ", Avenue: " << avenue[mid] << ")" << endl;
+
   }
   return 0;
 }
