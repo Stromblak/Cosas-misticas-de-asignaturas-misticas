757be556673e317cdc7a6f8c8ea5ed906492c85c
Stromblak <105810066+Stromblak@users.noreply.github.com>
2022-05-21 11:22:54 -0400

    Update gen.cpp

--- gen.cpp
+++ gen.cpp
@@ -1,2 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
 
-// rellenoooooooooo
+
+int main(){
+	int x, y, z = 0;
+	cin >> x >> y;
+	while( x > 0){
+		cout << x << " " << z << " " << y << endl;
+		if (x & 1) z = z + y;	
+		x = x>>1;		
+		y = y<<1;	
+	}	
+	
+	cout << z << endl;
+	
+	return 0;
+	
+}
+
+
