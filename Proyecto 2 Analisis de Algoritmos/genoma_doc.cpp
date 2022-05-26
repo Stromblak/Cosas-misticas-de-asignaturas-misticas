63e0ee8072df23c85720237f39aa7361725f7533
RyuLith <105754662+RyuLith@users.noreply.github.com>
2022-05-26 12:18:15 -0400

    Add files via upload

--- Proyecto 2 Analisis de Algoritmos/genoma_doc.cpp
+++ Proyecto 2 Analisis de Algoritmos/genoma_doc.cpp
@@ -24,26 +24,29 @@ typedef unsigned int uint;
 class HashPerfecto
 {
 private:
-	string genoma;
-	map<int, int> mapkmers;
-	vector<vector<int>> tabla;
-	int p, k, rep4, rep2, pc; // primo, 15, repeticiones 4n, repeticiones 2n
+	string genoma;			   // GCF_000820745.1_Polynesia_massiliensis_MS3_genomic.fna
+	map<int, int> mapkmers;	   // map kmer y numero de repeticiones
+	vector<vector<int>> tabla; // tabla hash de 2 niveles
+	int p, k, rep4, rep2, pc;  // primo, 15, repeticiones 4n, repeticiones 2n
 	int a, b, m, ai, bi, mi;
-	int h(int knum);
-	int hi(int knum);
-	int kmerToInt(string kmer);
-	void procesarkmers();
-	void crearTabla();
+	int h(int knum);			// hash tabla de primer nivel
+	int hi(int knum);			// hash tabla de segundo nivel
+	int kmerToInt(string kmer); // 15-mer a int
+	void procesarkmers();		// crear los kmers a partir del genoma
+	void crearTabla();			// hashing a la tabla
 	void porcentaje(int i, int j);
-	void tabla2n();
+	void tabla2n(); // hashing a la tabla, condicion 2n
 
 public:
-	HashPerfecto(string *gen);
-	int search(string kmer);
+	HashPerfecto(string *gen); // Constructor
+	int search(string kmer);   // buscar la posicion????? del kmer
 };
+
 /**
  * @brief Construct a new Hash Perfecto:: Hash Perfecto object
- *
+ * Para empezar, primero se deben inicializar todos los valores necesarios.
+ * Luego, se deben crear los kmers que seran almacenados mediante hashing a una tabla
+ * Finalmente, se hace hash a los kmers y se crea la tabla hash
  * @param gen genoma
  */
 HashPerfecto::HashPerfecto(string *gen)
@@ -51,7 +54,7 @@ HashPerfecto::HashPerfecto(string *gen)
 	srand(time(NULL));
 	rand();
 	genoma = *gen;
-	p = 7595479;
+	p = 7595479; // primo mas despues de .....?
 	k = 15;
 	rep4 = rep2 = 1;
 	pc = 10;
@@ -63,37 +66,45 @@ HashPerfecto::HashPerfecto(string *gen)
 	cout << "Repeticiones 4n: " << rep4 << endl;
 	cout << "Repeticiones 2n: " << rep2 << endl;
 }
+
 /**
- * @brief Hashing perfecto primera tabla
- *
+ * @brief Hashing tabla de primer nivel
+ * Usar la siguiente familia de funciones hash universales. Donde
+ * p es un numero primo, a y b son valores aleatorios en el rango [0..p − 1],
+ * y m el numero de buckets de la tabla.
+ * h(k) = ((a × k + b)mod p)mod m)
+
  * @param knum (int)kmer
- * @return bucket
+ * @return bucket, primer nivel
  */
 int HashPerfecto::h(int knum)
 {
 	return ((uint)(a * knum + b) % p) % m;
 }
+
 /**
- * @brief Hashing perfecto segunda tabla
+ * @brief Hashing tabla de segundo nivel
  *
  * @param knum (int)kmer
- * @return bucket, donde se almacenara el kmer
+ * @return bucket, segundo nivel, donde se almacenara el kmer
  */
 int HashPerfecto::hi(int knum)
 {
 	return ((uint)(ai * knum + bi) % p) % mi;
 }
+
 /**
  * @brief Transformar el string kmer a int
  *
  * @param kmer kmer en string
- * @return (int)kmer
+ * @return kmer como int
  */
 int HashPerfecto::kmerToInt(string kmer)
 {
 	int knum = 0;
 	for (int i = 0; i < k; i++)
 	{
+		/*
 		if (kmer[i] == 'A')
 			knum += 0 << i * 2;
 		else if (kmer[i] == 'C')
@@ -102,13 +113,32 @@ int HashPerfecto::kmerToInt(string kmer)
 			knum += 2 << i * 2;
 		else if (kmer[i] == 'G')
 			knum += 3 << i * 2;
+		*/
+		// le puse un case switch pa que se viera mas bonito, no se si lo queri dejar asi o como antes
+		// (no lo he probrado)
+		switch (kmer[i])
+		{
+		case 'A':
+			knum += 0 << i * 2;
+			break;
+		case 'C':
+			knum += 1 << i * 2;
+			break;
+		case 'T':
+			knum += 2 << i * 2;
+			break;
+		case 'G':
+			knum += 3 << i * 2;
+			break;
+		}
 	}
 	return knum;
 }
+
 /**
  * @brief Mapear todos los 15-mers
- * Se elige parte del string genoma para el 15-mer y luego se transforma a int.
- * Despues, se almacena el 15-mer (int) en un map
+ * Se elige un pedazo de largo 15 del string genoma para el 15-mer, y luego se transforma a int.
+ * Despues, se almacena el 15-mer (int) en un map.
  * Se utiliza la funcion substr(i, k), ya que el 15-mer empiza en la posicion i, tiene un largo k = 15
  */
 void HashPerfecto::procesarkmers()
@@ -127,15 +157,17 @@ void HashPerfecto::procesarkmers()
 		 << endl;
 	;
 }
+
 /**
  * @brief Creacion de las 2 tablas
- *
+ * Tabla de primer nivel: Para determinar funcion hash, h, de primer nivel.
+ * Elegir m = n, p > |S|, y a y b aleatoriamente en un rango [0..p − 1].
  */
 void HashPerfecto::crearTabla()
 {
 	auto start = chrono::high_resolution_clock::now();
 
-	vector<vector<int>> tablaAux(m); // tabla de tamaño m
+	vector<vector<int>> tablaAux(m); // tabla, primer nivel tamaño m
 	tabla = vector<vector<int>>(m);
 
 	cout << "Creando tabla de primer nivel" << endl;
@@ -143,14 +175,18 @@ void HashPerfecto::crearTabla()
 	{
 		a = (rand() + rand() << 15) % p; // valor aleatorio entre [0..p − 1]
 		b = (rand() + rand() << 15) % p; // valor aleatorio entre [0..p − 1]
+
 		for (auto kmer : mapkmers)
-			tablaAux[h(kmer.first)].push_back(kmer.first); // hash kmer e insertarlo en la tabla
-		int sum = 0;									   // sumatoria de i = 0 hasta m-1 de ci^2
-		for (auto c : tablaAux)							   // 0 hasta m-1
-			sum += c.size() * c.size();					   // ci^2
-		if (sum < 4 * m)								   // funcion hash buena
+			// hash kmer e insertarlo en una tabla de 2 niveles auxiliar
+			tablaAux[h(kmer.first)].push_back(kmer.first);
+
+		// Condicion de sumatoria de i = 0 hasta m-1 de ci^2 < 4n
+		int sum = 0;					// sumatoria de i = 0 hasta m-1 de ci^2
+		for (auto c : tablaAux)			// 0 hasta m-1
+			sum += c.size() * c.size(); // ci^2
+		if (sum < 4 * m)				// funcion hash buena
 			break;
-		else
+		else // repetir y aumentar contador
 			rep4++;
 	}
 
@@ -167,15 +203,23 @@ void HashPerfecto::crearTabla()
 			bi = (rand() + rand() << 15) % p;
 			tabla[i] = vector<int>(3 + mi); // 3 espacios para almacenar ai, bi, mi
 
-			for (int knum : tablaAux[i])
-			{
+			// recorrer tabla auxiliar de segundo nivel (del bucket i de la tabla de primer niveL)
+			// hacer hash a los elementos de esta tabla, a la tabla real
+			// el hash de primer nivel se hizo anteriormente, obteniendo i 
+			// ahora se debe hacer hash a los elementos en la tabla de segundo nivel
+			for (int knum : tablaAux[i]) 
+			{	
+				// si ya existe un elemento (colision)
 				if (tabla[i][3 + hi(knum)])
 				{
 					flag = 1;
 					break;
 				}
 				else
-					tabla[i][3 + hi(knum)] = mapkmers[knum];
+					tabla[i][3 + hi(knum)] = mapkmers[knum]; // deberiamos almacenar el kmer aqui o no??
+					// sgun tengo entendido, se almacena el elemento con el hash
+					// para dps usar search() y buscar la posicion de ese elemento
+					// asi queda con tiempo O(1)
 			}
 			// tripleta ai, bi, mi
 			if (!flag)
@@ -246,7 +290,8 @@ void HashPerfecto::tabla2n()
  */
 int HashPerfecto::search(string kmer)
 {
-	if(kmer.size() != k) return -1;
+	if (kmer.size() != k)
+		return -1;
 	int knum = kmerToInt(kmer); // x
 	int pos = h(knum);			// h(x)
 
@@ -268,28 +313,36 @@ int main()
 	cout << "Ingresar numero o nombre del archivo: ";
 	cin >> nombre;
 
-	if(nombre == "1") nombre =  "GCF_000820745.1_Polynesia_massiliensis_MS3_genomic.fna";
-	if(nombre == "2") nombre =  "t.txt";
+	if (nombre == "1")
+		nombre = "GCF_000820745.1_Polynesia_massiliensis_MS3_genomic.fna";
+	if (nombre == "2")
+		nombre = "t.txt";
 
 	ifstream archivo(nombre);
 	string genoma;
 	int copiar = 1;
-	while(true){
+	while (true)
+	{
 		string data;
 		archivo >> data;
-		if(data.empty()) break;
+		if (data.empty())
+			break;
 
-		if(data[0] == '>') copiar = 0;
-		if(copiar) genoma += data;
-		if(data == "sequence") copiar = 1;
+		if (data[0] == '>')
+			copiar = 0;
+		if (copiar)
+			genoma += data;
+		if (data == "sequence")
+			copiar = 1;
 	}
-	
+
 	archivo.close();
 	HashPerfecto h = HashPerfecto(&genoma);
 
 	string kmer;
 	cout << "Ingresar 15-mer a buscar, 0 para salir" << endl;
-	while(kmer != "0"){
+	while (kmer != "0")
+	{
 		cin >> kmer;
 		cout << "Cantidad: " << h.search(kmer) << endl;
 	}
