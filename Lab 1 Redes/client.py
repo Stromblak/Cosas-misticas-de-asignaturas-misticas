bf29456170cc5d35bffd3b1f1f3ab004949a6fdd
RyuLith <105754662+RyuLith@users.noreply.github.com>
2022-06-01 16:10:40 -0400

    Add files via upload

---
+++ Lab 1 Redes/client.py
@@ -0,0 +1,15 @@
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+filename = input("Nombre del archivo: ")
+with open(filename, "r") as f:
+    contents = f.read()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(contents.encode())
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
