2761cce918a2eb2de686fc4b0c913538e0efd61e
RyuLith <105754662+RyuLith@users.noreply.github.com>
2022-06-04 12:18:41 -0400

    Add files via upload

--- Lab 1 Redes/client.py
+++ Lab 1 Redes/client.py
@@ -5,12 +5,15 @@ PORT = 80
 
 # filename = input("Nombre del archivo: ")
 filename = "data.txt"
-
 with open(filename, "r") as f:
-	contents = f.read()
-	f.close()
+    contents = f.read()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.connect((HOST, PORT))
-	s.sendall(contents.encode())
-	s.close()						# termina la conexion
+    s.connect((HOST, PORT))
+    # Enviar mensaje
+    print("Enviando el archivo", filename, "con tamaño")
+    s.sendall(contents.encode())
+
+
+
+# los with hacen que no sea necesario poner los close
