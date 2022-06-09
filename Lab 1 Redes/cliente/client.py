33c95b1a27e8bdd3ed094c85c7ade06251b27585
RyuLith <105754662+RyuLith@users.noreply.github.com>
2022-06-09 13:38:56 -0400

    Add files via upload

--- Lab 1 Redes/cliente/client.py
+++ Lab 1 Redes/cliente/client.py
@@ -1,48 +1,57 @@
 import socket
 import os
+import sys
 import encriptacion as enc
 
+
 def client(host, port, cifrado, filename):
-	print("Cliente inicializado")
+    print("Cliente inicializado")
 
-	filepath = "cliente\\" + filename
-	with open(filepath, "r") as f:
-		contenido = f.read()
+    filepath = "cliente\\" + filename
+    with open(filepath, "r") as f:
+        contenido = f.read()
 
-	total = len(contenido)
-	filesize = round(os.path.getsize(filepath) / (1000 ** 2), 3)
-	stats = str(filename) + '|' + str(filesize) + '|' + str(total) + '|'
-	contenido = stats + contenido
+    total = len(contenido)
+    filesize = round(os.path.getsize(filepath) / (1000 ** 2), 3)
+    stats = str(filename) + '|' + str(filesize) + '|' + str(total) + '|'
+    contenido = stats + contenido
 
-	total = 0
-	data = []
-	if cifrado == 0: n = 512
-	if cifrado == 1: n = 512
-	if cifrado == 2: n = 496
+    total = 0
+    data = []
+    if cifrado == 0:
+        n = 512
+    if cifrado == 1:
+        n = 512
+    if cifrado == 2:
+        n = 496
 
-	for i in range(0, len(contenido), n):
-		if cifrado == 0: 
-			dataEnc = contenido[i:i+n].encode()
-		if cifrado == 1: 
-			dataEnc = enc.encrypt_sim(contenido[i:i+n])
-		if cifrado == 2: 
-			dataEnc = enc.encrypt_asim(contenido[i:i+n])
-		total += len(dataEnc)
-		data.append(dataEnc)
+    for i in range(0, len(contenido), n):
+        if cifrado == 0:
+            dataEnc = contenido[i:i+n].encode()
+        if cifrado == 1:
+            dataEnc = enc.encrypt_sim(contenido[i:i+n])
+        if cifrado == 2:
+            dataEnc = enc.encrypt_asim(contenido[i:i+n])
+        total += len(dataEnc)
+        data.append(dataEnc)
 
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		print("Estableciendo la conexion... ", end = '')
-		if not s.connect_ex((host, port)):
-			print("Conexion exitosa!")
-		else:
-			print("Conexion fallida")
-			print("Cerrando cliente")
-			return
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        print("Estableciendo la conexion... ", end='')
+        if not s.connect_ex((host, port)):
+            print("Conexion exitosa!")
+        else:
+            print("Conexion fallida")
+            print("Cerrando cliente")
+            return
 
-		# aqui supongo que se pueden indicar errores ??
-		print(f"Enviando el archivo {filename} de tamaño {filesize} MB.")
-		enviado = 0
-		for d in data:
-			s.sendall(d)
-			enviado += len(d)
-			print(f"Progreso: {round(100*enviado/total,2)}%")
+        # aqui supongo que se pueden indicar errores ??
+        print(f"Enviando el archivo {filename} de tamaño {filesize} MB.")
+        enviado = 0
+        for d in data:
+            try:
+                s.sendall(d)
+            except socket.error:
+                print("Error enviando el archivo")
+                sys.exit(1)
+            enviado += len(d)
+            print(f"Progreso: {round(100*enviado/total,2)}%")
