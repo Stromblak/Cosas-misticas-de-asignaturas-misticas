c9ae1a9fbd2181006519892036eedd5e0ee1cb44
RyuLith <105754662+RyuLith@users.noreply.github.com>
2022-06-09 13:38:22 -0400

    Add files via upload

--- Lab 1 Redes/server/server.py
+++ Lab 1 Redes/server/server.py
@@ -1,46 +1,72 @@
 import socket
+import sys
 import encriptacion as enc
 
+
 def server(host, port, modo):
-	print("Servidor inicializado")
+    print("Servidor inicializado")
 
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		# Bind socket, con la direccion y el puerto
-		s.bind((host, port))
-		# Listen
-		s.listen()
-		print("Esperando conexiones")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            # Bind socket, con la direccion y el puerto
+            s.bind((host, port))
+            # Listen
+            s.listen()
+            print("Esperando conexiones")
+        except socket.error:
+            print("Error")
+            sys.exit(1)
 
-		while True:
-			clientsocket, address = s.accept()
+        while True:
+            try:
+                clientsocket, address = s.accept()
+            except socket.error:
+                print("Error")
+                sys.exit(1)
 
-			with clientsocket as c:
-				print(f"Conexion entrante: {address}")
+            with clientsocket as c:
+                print(f"Conexion entrante: {address}")
 
-				if modo == 0: data = c.recv(512).decode().split("|", 3)
-				if modo == 1: data = enc.decrypt_sim( c.recv(512) ).split("|", 3)
-				if modo == 2: data = enc.decrypt_asim( c.recv(512) ).split("|", 3)
-				filename = data.pop(0)
-				filesize = float(data.pop(0))
-				total = int(data.pop(0))
-				filepath = "server\\" + filename
-				recibido = len(data[0])
+                try:
+                    if modo == 0:
+                        data = c.recv(512).decode().split("|", 3)
+                    if modo == 1:
+                        data = enc.decrypt_sim(c.recv(512)).split("|", 3)
+                    if modo == 2:
+                        data = enc.decrypt_asim(c.recv(512)).split("|", 3)
+                except socket.error:
+                    print(f"Error al recibir el archivo")
+                    sys.exit(1)
 
-				print(f"Recibiendo el archivo {filename} de tamaño {filesize} MB.")
-				print(f"Progreso: {round(100*recibido/total,2)}%")	
-				while True:
-					buffer = c.recv(512)
-					if not buffer:
-						break
-					if modo == 0: bufferDec = buffer.decode()
-					if modo == 1: bufferDec = enc.decrypt_sim(buffer)
-					if modo == 2: bufferDec = enc.decrypt_asim(buffer)
-					data.append(bufferDec)
-					recibido += len(bufferDec)
+                filename = data.pop(0)
+                filesize = float(data.pop(0))
+                total = int(data.pop(0))
+                filepath = "server\\" + filename
+                recibido = len(data[0])
 
-					print(f"Progreso: {round(100*recibido/total,2)}%")
+                print(
+                    f"Recibiendo el archivo {filename} de tamaño {filesize} MB.")
+                print(f"Progreso: {round(100*recibido/total,2)}%")
+                while True:
+                    try:
+                        buffer = c.recv(512)
+                    except socket.error:
+                        print(f"Error al recibir el archivo")
+                        sys.exit(1)
+                    if not buffer:
+                        break
+                    if modo == 0:
+                        bufferDec = buffer.decode()
+                    if modo == 1:
+                        bufferDec = enc.decrypt_sim(buffer)
+                    if modo == 2:
+                        bufferDec = enc.decrypt_asim(buffer)
+                    data.append(bufferDec)
+                    recibido += len(bufferDec)
 
-				with open(filepath, "w") as r:
-					r.write(''.join(data))
+                    print(f"Progreso: {round(100*recibido/total,2)}%")
 
-				print(f"Conexion finalizada\n")
+                with open(filepath, "w") as r:
+                    r.write(''.join(data))
+
+                print(f"Conexion finalizada\n")
