9088de151cc2a9c20e8b452927a6274c861f54ea
Stromblak <105810066+Stromblak@users.noreply.github.com>
2022-06-09 11:00:03 -0400

    Add files via upload

--- Lab 1 Redes/main.py
+++ Lab 1 Redes/main.py
@@ -1,6 +1,7 @@
 from cliente import client
 from server import server
 import socket
+import os
 
 host = socket.gethostname()
 port = 800
@@ -8,15 +9,29 @@ port = 800
 print("1: Servidor")
 print("2: Cliente")
 modo = int(input("Ingresar modo: "))
+
+while modo != 1 and modo != 2:
+	print("Input incorrecto")
+	modo = int(input("Ingresar modo: "))
 print()
 
 print("1: Encriptacion simetrica")
 print("2: Encriptacion asimetrica")
-enc = int(input("Ingresar tipo de encriptacion: "))
+cifrado = int(input("Ingresar tipo de encriptacion: "))
+
+while cifrado != 1 and cifrado != 2:
+	print("Input incorrecto")
+	cifrado = int(input("Ingresar tipo de encriptacion: "))
 print()
 
 if modo == 1:
-	server.server(host, port, enc)
-elif modo == 2:
+	server.server(host, port, cifrado)
+if modo == 2:
 	archivo = input("Ingresar nombre de archivo: ")
-	client.client(host, port, enc, archivo)
+
+	while not os.path.exists("cliente\\" + archivo):
+		print("Archivo no encontrado")
+		archivo = input("Ingresar nombre de archivo: ")
+	print()
+
+	client.client(host, port, cifrado, archivo)
