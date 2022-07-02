44dd30e4fbbf5a631ac9db287efb5c9fe37bed12
Stromblak <105810066+Stromblak@users.noreply.github.com>
2022-07-02 14:14:16 -0400

    Add files via upload

--- Lab 2 Redes/rudp.py
+++ Lab 2 Redes/rudp.py
@@ -8,27 +8,30 @@ from Crypto.Cipher import AES
 buffersize = 1024
 
 # https://github.com/jorgejarai/redes_udec/tree/main/lab1-2_rtt/python
-# cambie algunas cosas, y me gustaria desacerme del rtt porque siento que es innecesario xD
-# tambien hacerle un rework a los intentos de envio xD
+# inicios de funciones y atributos
+#  publico
+# _ protegido
+# __ privado
+
 
 class Datagram:
 	def __init__(self, payload, address, sequence_no):
-		self.payload = payload
-		self.address = address
-		self.sequence_no = sequence_no
+		self._payload = payload
+		self._address = address
+		self._sequence_no = sequence_no
 
 class cifrado:
 	def __init__(self, key):
-		self.iv = 'This is an IV456'.encode()
-		self.hashKey = hashlib.sha256( key.encode() ).digest()
-		self.mode = AES.MODE_CFB
+		self.__iv = 'This is an IV456'.encode()
+		self.__hashKey = hashlib.sha256( key.encode() ).digest()
+		self.__mode = AES.MODE_CFB
 
 	def encrypt(self, pickle):	
-		cipher = AES.new(self.hashKey, self.mode, self.iv)
+		cipher = AES.new(self.__hashKey, self.__mode, self.__iv)
 		return cipher.encrypt( pickle )
 
 	def decrypt(self, cipherPickle):
-		cipher = AES.new(self.hashKey, self.mode, self.iv)
+		cipher = AES.new(self.__hashKey, self.__mode, self.__iv)
 		return cipher.decrypt( cipherPickle )
 
 
@@ -42,12 +45,12 @@ class RUDPServer(cifrado):
 			sys.exit(1)
 
 	def receive(self):
-		data, address = self.s.recvfrom( buffersize )
-		datagram = pickle.loads(data)
+		pickleData, address = self.s.recvfrom( buffersize )
+		datagram = pickle.loads(pickleData)
 
-		self.last_seqno = datagram.sequence_no
+		self.last_seqno = datagram._sequence_no
 
-		return (datagram.payload, address)
+		return (datagram._payload, address)
 
 	def reply(self, address, payload):
 		datagram = Datagram(payload, address, self.last_seqno)
@@ -88,12 +91,12 @@ class RUDPClient(cifrado):
 				except BlockingIOError:
 					continue
 
-				if recvDatagram.sequence_no == self.sequence_no:
+				if recvDatagram._sequence_no == self.sequence_no:
 					send = True
 					break
 
 		if send:
-			return recvDatagram.payload
+			return recvDatagram._payload
 		else:
 			pass
 			# error?		
