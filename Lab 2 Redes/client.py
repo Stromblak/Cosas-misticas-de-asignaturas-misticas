94c1b6ab4537b516372de9d29c3f559b05864a6c
Stromblak <105810066+Stromblak@users.noreply.github.com>
2022-07-02 22:15:45 -0400

    Add files via upload

--- Lab 2 Redes/client.py
+++ Lab 2 Redes/client.py
@@ -2,38 +2,34 @@ from rudp import RUDPClient
 
 
 client = RUDPClient("localhost", 25565)
+archivos = client.send_recv('~')
 
 print('(carpeta): Moverse a la carpeta')
 print('(archivo): Descargar archivo')
 print('..       : Retroceder')
 print('~        : Volver al inicio')
 print('close    : Salir')
+print()
 
-
-
-archivos = client.send_recv('~')
-print( archivos )
 
 while True:
+	print( '    '.join(archivos) )
 	accion = input()
+	print()
 
 	if accion in ['..', '~']:
 		archivos = client.send_recv( accion )
-		print( archivos )
-	
+		
 	elif accion in archivos:
-		if accion.split('.')[-1] in ['txt', 'bin']:
+		if accion.split('.')[-1] in ['txt', 'bin', 'py']:
 			client.send_recv( accion )
 			break
 			# pasar a recivir archivo
-		else:
+		else: 
 			archivos = client.send_recv( accion )
-			print( archivos )
 
 	elif accion == 'close':
 		break
+
 	
-	else:
-		print('Input incorrecto')
-		continue
 
