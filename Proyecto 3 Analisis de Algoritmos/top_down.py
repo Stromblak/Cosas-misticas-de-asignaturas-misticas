20fe8aab5bd9ec80621f24f4ee0e392f72f8c134
Stromblak <105810066+Stromblak@users.noreply.github.com>
2022-07-09 22:26:43 -0400

    Add files via upload

--- Proyecto 3 Analisis de Algoritmos/top_down.py
+++ Proyecto 3 Analisis de Algoritmos/top_down.py
@@ -1,57 +1,56 @@
 
 
 
-def top_down(i, j, M, X, Y):
+def top_down(i, j, M, X, Y, B):
 	if M[i][j] == float('inf'):
 		if i == 0 or j == 0:
 			M[i][j] = -2*(i+j)
 		else:
 			if X[i-1] == Y[j-1]: r = 1
 			else: r = -1
-			m1 = top_down(i-1, j  , M, X, Y) - 2
-			m2 = top_down(i  , j-1, M, X, Y) - 2
-			m3 = top_down(i-1, j-1, M, X, Y) + r
+			m1 = top_down(i-1, j  , M, X, Y, B) - 2
+			m2 = top_down(i  , j-1, M, X, Y, B) - 2
+			m3 = top_down(i-1, j-1, M, X, Y, B) + r
+			
 			M[i][j] = max(m1, m2, m3)
+			if M[i][j] == m1: B[i][j] = (i-1, j)
+			elif M[i][j] == m2: B[i][j] = (i, j-1)
+			else: B[i][j] = (i-1, j-1)
 
 	return M[i][j]
 
 
-X = 'GAAC'
-Y = 'CAAGAC'
+X = 'CCAAT T AG'
+Y = 'CAAT T T G'
 i = len(X)
 j = len(Y)
 
-
 M = [ [float('inf')]*(j+1) for _ in range(i+1) ]
+B = [ [(0, 0)]*(j+1) for _ in range(i+1) ]
 
-top_down(i, j, M, X, Y)
+
+top_down(i, j, M, X, Y, B)
 
 a = []
 b = []
-a.append(X[i-1])
-b.append(Y[j-1])
-
 while i != 0 or j != 0:
-	m = max( M[i-1][j], M[i][j-1], M[i-1][j-1] )
-	if m == M[i-1][j]:
-		i = i-1
-		a.append(X[i-1])
-		b.append('-')
-
-	if m == M[i][j-1]:
-		j = j-1
+	if B[i][j][0] == i:
 		a.append('-')
-		b.append(Y[j-1])
-
-	if m == M[i-1][j-1]:
-		i = i-1
-		j = j-1
+	else:
 		a.append(X[i-1])
+
+	if B[i][j][1] == j:
+		b.append('-')
+	else:
 		b.append(Y[j-1])
 		
-for m in M:
-	print(m)
+	i, j = B[i][j]
 
-print(''.join(reversed(b)), "CAAGAC")
-print(''.join(reversed(a)), "GAA--C")
+	
 
+
+print(''.join(reversed(b)))
+print('C-AAT T T G')
+print()
+print(''.join(reversed(a)))
+print('CCAAT T A-G')
